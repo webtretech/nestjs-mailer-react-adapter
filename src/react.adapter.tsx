07e0ab66d39dc62a362c32dc1307a00d82979809
Lawrence Onah <paplow01@gmail.com>
2024-09-14 10:23:49 +0100

    refactor: update templateName variable assignment

--- src/react.adapter.tsx
+++ src/react.adapter.tsx
@@ -19,7 +19,7 @@ export class ReactAdapter implements TemplateAdapter {
   public compile(mail: any, callback: any, options: MailerOptions): void {
     const { context, template } = mail.data;
     const templateExt = path.extname(template) || ".js";
-    const templateName = path.basename(template, path.extname(template));
+    const templateName = path.basename(template, templateExt);
     const templateDir = path.isAbsolute(template)
       ? path.dirname(template)
       : path.join(options.template.dir, path.dirname(template));
