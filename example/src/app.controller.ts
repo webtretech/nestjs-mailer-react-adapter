b199074ec019aee1b23c6f5dab07bfd47ad6d815
Lawrence Onah <paplow01@gmail.com>
2024-10-12 10:31:35 +0100

    feat: implement welcome email sending feature

--- example/src/app.controller.ts
+++ example/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  welcome(): Promise<string> {
+    return this.appService.sendWelcomeMail();
   }
 }
--- example/src/app.module.ts
+++ example/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ReactAdapter } from '@webtre/nestjs-mailer-react-adapter';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.domain.com',
+        port: 2525,
+        secure: false,
+        auth: {
+          user: 'user@domain.com',
+          pass: 'password',
+        },
+      },
+      defaults: {
+        from: '"Webtre Technologies" <hello@domain.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new ReactAdapter(),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
